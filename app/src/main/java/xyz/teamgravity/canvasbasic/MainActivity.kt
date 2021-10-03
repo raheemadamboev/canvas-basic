57b0bcb46dba1a45a1da766ea1f786782f30b84d
Raheem <raheemideadev@gmail.com>
2021-10-03 15:55:03 +0500

    draw black rectangle

---
+++ app/src/main/java/xyz/teamgravity/canvasbasic/MainActivity.kt
@@ -0,0 +1,35 @@
+package xyz.teamgravity.canvasbasic
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyCanvas()
+        }
+    }
+}
+
+@Composable
+fun MyCanvas() {
+    Canvas(
+        modifier = Modifier
+            .padding(20.dp)
+            .size(300.dp)
+    ) {
+        drawRect(
+            color = Color.Black,
+            size = size
+        )
+    }
+}
