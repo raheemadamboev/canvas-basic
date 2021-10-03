2f981760a55d2684209c8140edc522be304049f2
Raheem <raheemideadev@gmail.com>
2021-10-03 16:01:05 +0500

    draw circle and draw stroke rect

--- app/src/main/java/xyz/teamgravity/canvasbasic/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/canvasbasic/MainActivity.kt
@@ -8,7 +8,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
@@ -30,6 +34,24 @@ fun MyCanvas() {
         drawRect(
             color = Color.Black,
             size = size
+        )
+
+        drawRect(
+            color = Color.Red,
+            topLeft = Offset(100f, 100f),
+            size = Size(100f, 100f),
+            style = Stroke(
+                width = 3.dp.toPx()
+            )
+        )
+
+        drawCircle(
+            brush = Brush.radialGradient(
+                colors = listOf(Color.Red, Color.Yellow),
+                center = center,
+                radius = 100f
+            ),
+            radius = 100f
         )
     }
 }
