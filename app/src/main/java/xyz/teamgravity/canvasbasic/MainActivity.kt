94026e46857c11795d757836d0566f5b3684ec89
Raheem <raheemideadev@gmail.com>
2021-10-03 16:04:40 +0500

    draw arc

--- app/src/main/java/xyz/teamgravity/canvasbasic/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/canvasbasic/MainActivity.kt
@@ -53,5 +53,17 @@ fun MyCanvas() {
             ),
             radius = 100f
         )
+
+        drawArc(
+            color = Color.Green,
+            startAngle = 0f,
+            sweepAngle = 270f,
+            useCenter = false,
+            topLeft = Offset(100f, 500f),
+            size = Size(200f, 200f),
+            style = Stroke(
+                width = 3.dp.toPx()
+            )
+        )
     }
 }
